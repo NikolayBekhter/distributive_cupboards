9c036c7a6719bf4dc27d013c5564c812b01ecc95
BekhterNikolay <n.v.bekhter@mail.ru>
2023-01-20 21:01:54 +0300

    Initial commit

---
+++ src/main/java/ru/gentle/distributive/cupboard/DistributiveCupboardsApplication.java
@@ -0,0 +1,13 @@
+package ru.gentle.distributive.cupboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributiveCupboardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributiveCupboardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/gentle/distributive/cupboard/DistributiveCupboardsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gentle.distributive.cupboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributiveCupboardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
